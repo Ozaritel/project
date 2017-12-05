94db871fc4e75126f86a8e1bfe8b39399386be34
Ozaritel <maximfedorov98@gmail.com>
2017-12-05 17:52:48 +0300

    Index
    Главная страница

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $title = 'STEAMSELLS.COM - Магазин самых дешевых игр STEAM, ORIGIN, UPLAY';
+    require "header.php";
+    require "live.php";
+    require "randoms.php";
+    require "items/items.php";
+?>
+
+<?php
+    require "footer.php";
+?>
