cb1d3220b30b0bab5ea2d34ed93226cbb4c28ed1
Ozaritel <maximfedorov98@gmail.com>
2017-12-07 19:56:50 +0300

    bd connection

--- admin/add.php
+++ admin/add.php
@@ -2,6 +2,8 @@
 include "../datebase/config.php";
   try {
     $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->exec("set names utf8");
   }
   catch(PDOException $e) {
     echo $e->getMessage();
