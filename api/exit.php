610de54eb81f4c509ba93b180f93d8b2e165f32a
Ozaritel <maximfedorov98@gmail.com>
2018-12-06 19:58:38 +0200

    Delete last

--- api/exit.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-  session_start();
-  unset($_SESSION['userid']);
-  session_destroy();
-  header("Location:../index.php");
-?>
--- datebase/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-   	$dbname = "ozaritel1";
-	$password = "As1ka2fen3";
-	$host = "mysql.zzz.com.ua";
-	$user = "ozaritel";
-?>
