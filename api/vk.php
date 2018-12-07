a968faecfe0c5bd4702a3e83199c1dc7eb82f261
Ozaritel <maximfedorov98@gmail.com>
2018-12-07 10:16:41 +0200

    vk

--- api/vk.php
+++ api/vk.php
@@ -7,7 +7,7 @@ include "../datebase/config.php";
 $token = json_decode(file_get_contents('https://oauth.vk.com/access_token?client_id='.ID.'&redirect_uri='.URL.'&client_secret='.SECRET.'&code='.$_GET['code']),true);
 if(!$token)
 	exit("Токен не верный");
-$data = json_decode(file_get_contents('https://api.vk.com/method/users.get?user_id='.$token['user_id'].'&access_token='.$token['access_token'].'&fields=uid,first_name,last_name,photo_big&v=5.73'),true);
+/*$data = json_decode(file_get_contents('https://api.vk.com/method/users.get?user_id='.$token['user_id'].'&access_token='.$token['access_token'].'&fields=uid,first_name,last_name,photo_big&v=5.73'),true);
 if(!$data)
 	exit("Токен не верный");
 $data = $data['response'][0];
@@ -15,7 +15,7 @@ try {
   $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $db->exec("set names utf8");
-}
+}*/
 catch(PDOException $e) {
     echo $e->getMessage();
 }
